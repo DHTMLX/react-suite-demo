62218da321471b59823894dc9d77bffa2d2813b4
vally <vally.pepyako@gmail.com>
2019-07-09 16:47:01 +0300

    add todo

--- src/About.js
+++ src/About.js
@@ -8,6 +8,7 @@ export default class About extends Component {
 	render() {
 		return (
 			<div style={{textAlign: 'center'}}>
+				{/* TODO: add more text or list of components */}
 				<h1 style={{color: 'rgba(0,0,0, 0.7)', fontWeight: '500'}}>DHX - react sample examples</h1>
 			</div>
 		)
