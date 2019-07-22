5f88cf230bf58e08a1ab308b403accd9dbed46ca
vally <vally.pepyako@gmail.com>
2019-07-22 18:01:54 +0300

    add message sample

---
+++ src/message/MessageCdn.js
@@ -0,0 +1,62 @@
+
+import React, { Component } from 'react';
+import fromCDN from "from-cdn";
+
+class MessageCDN extends Component {
+  constructor(props) {
+    super(props);
+
+    this.ready = fromCDN([
+      "https://cdn.dhtmlx.com/suite/edge/suite.js",
+      "https://cdn.dhtmlx.com/suite/edge/suite.css"
+    ]);
+  }
+
+  showAlert = () => {
+    this.ready.then(() => {
+      window.dhx.alert({
+        header: "DHTMLX Alert Box",
+        text: "With the help of DHTMLX components you are able to create versatile user interfaces for web apps of any kind. Modern technologies and design make our library a great tool for developers working on the most complex projects. And here dhtmlxMessage represents a small but indispensable toolkit of helpers for initializing all types of messages: notifications, alert and confirmation boxes, and tooltips. Try out the abilities of dhtmlxMessage in our interactive demos and samples.",
+        buttonsAlignment: "center",
+        buttons: ["ok"]
+      })
+    });
+  }
+  showConfirm = () => {
+    this.ready.then(() => {
+      window.dhx.confirm({
+        header: "DHTMLX Confirm Box",
+        text: "With the help of DHTMLX components you are able to create versatile user interfaces for web apps of any kind. Modern technologies and design make our library a great tool for developers working on the most complex projects. And here dhtmlxMessage represents a small but indispensable toolkit of helpers for initializing all types of messages: notifications, alert and confirmation boxes, and tooltips. Try out the abilities of dhtmlxMessage in our interactive demos and samples.",
+        buttonsAlignment: "center",
+      })
+    });
+  }
+  showMessage = () => {
+    this.ready.then(() => {
+      window.dhx.message({
+        text: "Here is dhtmlxMessage", 
+        icon: "dxi-close"
+      })
+    })
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div style={{
+        display: 'flex'
+      }}>
+        <button className="button" onClick={() => this.showAlert()}>Show alert</button>
+        <button className="button" onClick={() => this.showConfirm()}>Show confirm</button>
+        <button className="button" onClick={() => this.showMessage()}>Show message</button>
+      </div>
+    );
+  }
+}
+MessageCDN.propTypes = {
+
+};
+export default MessageCDN;
---
+++ src/message/MessagePage.js
@@ -0,0 +1,49 @@
+
+import React, { Component } from 'react'
+import Message from './Message';
+import MessageCdn from './MessageCdn';
+
+export default class MessagePage extends Component {
+
+	componentDidMount() {
+		this.props.handleToolbarNavItems([...this.el.querySelectorAll('section')].map(item => item.id))
+	}
+	handleAnchorClick(e, id) {
+		e.preventDefault()
+		this.props.setActiveExapmle(id)
+	}
+	render() {
+		return (
+			<main ref={(el) => this.el = el}>
+				<section className="hgroup active" id="basic">
+					<h3>
+						NPM basic inintialization 
+						<a href="#basic" className="anchor" onClick={(e) => this.handleAnchorClick(e, 'basic')}>
+							<img src="/static/link.svg" alt="" />
+						</a>
+					</h3>
+					<p>
+						<a href="https://github.com/DHTMLX/react-widgets/blob/master/src/message/Message.js" target="_blank"  rel="noopener noreferrer" >Code example </a> 
+					</p>
+					<div style={{display: 'flex', justifyContent: 'center'}}>
+						<Message /> 
+					</div>
+				</section>
+				<section className="hgroup" id="cdn">
+					<h3>
+						CDN basic inintialization 
+						<a href="#cdn" className="anchor" onClick={(e) => this.handleAnchorClick(e, 'cdn')}>
+							<img src="/static/link.svg" alt="" />
+						</a>
+					</h3>
+					<p>
+						<a href="https://github.com/DHTMLX/react-widgets/blob/master/src/message/MessageCDN.js" target="_blank"  rel="noopener noreferrer" >Code example </a> 
+					</p>
+					<div style={{display: 'flex', justifyContent: 'center'}}>
+						<MessageCdn /> 
+					</div>
+				</section>
+			</main>
+		)
+	}
+}
