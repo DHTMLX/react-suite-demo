1b5dd6088c1041705648c4d116f6aa944fe818ee
vally <vally.pepyako@gmail.com>
2019-07-22 18:04:02 +0300

    add layout sample

---
+++ src/layout/LayoutConfigured.js
@@ -0,0 +1,26 @@
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Layout as LayoutDHX } from "dhx-suite";
+import "dhx-suite/codebase/suite.css";
+
+class LayoutConfigured extends Component {
+  componentDidMount() {
+    this.layout = new LayoutDHX(this.el, {
+			css: "dhx_widget--bordered",
+    });
+  }
+  componentWillUnmount() {
+    this.layout.destructor();
+  }
+  render() {
+    return (
+      <div ref={el => this.el = el}></div>
+    );
+  }
+}
+LayoutConfigured.propTypes = {
+  
+};
+
+export default LayoutConfigured;
---
+++ src/layout/LayoutPage.js
@@ -0,0 +1,81 @@
+
+import React, { Component } from 'react'
+import Layout from './Layout';
+import LayoutCdn from './LayoutCdn';
+
+import LayoutConfigured from './LayoutConfigured';
+import LayoutProps from './LayoutProps';
+
+export default class LayoutPage extends Component {
+
+	componentDidMount() {
+		this.props.handleToolbarNavItems([...this.el.querySelectorAll('section')].map(item => item.id))
+	}
+	handleAnchorClick(e, id) {
+		e.preventDefault()
+		this.props.setActiveExapmle(id)
+	}
+	render() {
+		return (
+			<main ref={(el) => this.el = el}>
+				<section className="hgroup active" id="basic">
+					<h3>
+						NPM basic inintialization 
+						<a href="#basic" className="anchor" onClick={(e) => this.handleAnchorClick(e, 'basic')}>
+							<img src="/static/link.svg" alt="" />
+						</a>
+					</h3>
+					<p>
+						<a href="https://github.com/DHTMLX/react-widgets/blob/master/src/layout/Layout.js" target="_blank"  rel="noopener noreferrer" >Code example </a> 
+					</p>
+					<div style={{display: 'flex', justifyContent: 'center'}}>
+						<Layout /> 
+					</div>
+				</section>
+				<section className="hgroup" id="cdn">
+					<h3>
+						CDN basic inintialization 
+						<a href="#cdn" className="anchor" onClick={(e) => this.handleAnchorClick(e, 'cdn')}>
+							<img src="/static/link.svg" alt="" />
+						</a>
+					</h3>
+					<p>
+						<a href="https://github.com/DHTMLX/react-widgets/blob/master/src/layout/LayoutCDN.js" target="_blank"  rel="noopener noreferrer" >Code example </a> 
+					</p>
+					<div style={{display: 'flex', justifyContent: 'center'}}>
+						<LayoutCdn /> 
+					</div>
+					
+				</section>
+				<section className="hgroup" id="pre">
+					<h3>
+						Pre-configured component
+						<a href="#pre" className="anchor" onClick={(e) => this.handleAnchorClick(e, 'pre')}>
+							<img src="/static/link.svg" alt="" />
+						</a>
+					</h3>
+					<p>
+						<a href="https://github.com/DHTMLX/react-widgets/blob/master/src/layout/LayoutConfigured.js" target="_blank"  rel="noopener noreferrer" >Code example </a> 
+					</p>
+					<div style={{display: 'flex', justifyContent: 'center'}}>
+						<LayoutConfigured />
+					</div>
+				</section>
+				<section className="hgroup" id="props">
+					<h3>
+						Configured using props
+						<a href="#props" className="anchor" onClick={(e) => this.handleAnchorClick(e, 'props')}>
+							<img src="/static/link.svg" alt="" />
+						</a>
+					</h3>
+					<p>
+						<a href="https://github.com/DHTMLX/react-widgets/blob/master/src/layout/LayoutProps.js" target="_blank"  rel="noopener noreferrer" >Code example </a> 
+					</p>
+					<div style={{display: 'flex', justifyContent: 'center'}}>
+						<LayoutProps />
+					</div>
+				</section>
+			</main>
+		)
+	}
+}
