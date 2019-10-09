3d18040ed70ff71cd759224670675c62eeca1df4
vally <vally.pepyako@gmail.com>
2019-10-09 13:17:58 +0300

    add window show to conf window example

--- src/window/WindowConfigured.js
+++ src/window/WindowConfigured.js
@@ -1,25 +1,31 @@
 
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Window as WindowDHX } from "dhx-suite";
 import "dhx-suite/codebase/suite.min.css";
 
 class WindowConfigured extends Component {
   componentDidMount() {
-    this.window = new WindowDHX(this.el, {
-			width: 440, 
-			height: 520, 
-			title: "Window",
-			closable: true,
-			html: "<p>Here is a neat and flexible JavaScript window system with a fast and simple initialization.</p><p>Inspect all the DHTMLX window samples to discover each and every feature.</p><img style='display: block; width: 200px; height: 200px; margin-top: 20px; margin-left: auto; margin-right: auto' src='./static/developer.svg'>"
-		});
+    const windowHtml = "<p>Here is a neat and flexible JavaScript window system with a fast and simple initialization.</p><p>Inspect all the DHTMLX window samples to discover each and every feature.</p><img style='display: block; width: 200px; height: 200px; margin-top: 20px; margin-left: auto; margin-right: auto' src='./static/developer.svg'>";
+    this.window = new WindowDHX({
+      width: 440, 
+      height: 520, 
+      title: "Window",
+      html: windowHtml,
+      closable: true
+    });
   }
   componentWillUnmount() {
     this.window.destructor();
   }
   render() {
     return (
-      <div ref={el => this.el = el}></div>
+			<Fragment>
+				<div ref={el => this.el = el}></div>
+				<button className="button" onClick={() => this.window.show()}>
+					Show Window 
+				</button>
+		</Fragment>
     );
   }
 }
